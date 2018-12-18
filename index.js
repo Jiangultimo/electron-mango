f4325f60bd949ae1456cfdaa2493aa3cf2f64c29
Neras <medianeras57@gmail.com>
2018-12-18 19:52:48 +0800

    🎉init

---
+++ index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  win.loadURL('http://localhost:3000/')
+  win.on('closed', function () {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (win === null) {
+    createWindow()
+  }
+})
