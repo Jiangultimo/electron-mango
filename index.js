6b6a23f69099b12a3da20be6f6b00b530224eeef
Neras <medianeras57@gmail.com>
2018-12-20 09:31:54 +0800

    use react-app-rewired

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-let win
+let win;
 
 function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600
-  })
-  win.loadURL('http://localhost:3000/')
+  });
+  win.loadURL('http://localhost:3000/');
   win.on('closed', function () {
-    win = null
-  })
+    win = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () {
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
