b60579c0c836e800e465dd37a3a0b9acc18f041b
Neras <medianeras57@gmail.com>
2018-12-21 23:20:21 +0800

    add ipcMain

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { app, BrowserWindow,ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 let win
 
-function createWindow () {
+function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1200,
+    height: 720
   })
   win.loadURL('http://localhost:3000/')
   win.on('closed', function () {
@@ -17,19 +17,21 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin' ) {
+  if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 ipcMain.on('reqaction', (event, arg) => {
-  switch(arg){
+  const { action } = arg
+  switch (action) {
     case 'exit':
-      safeExit=true;
-      app.quit();//退出程序
-      break;
+      safeExit = true
+      app.quit() //退出程序
+      break
     case 'addDb':
       console.log(arg)
-      break;
+      event.sender.send('resaction', 'connected successfully')
+      break
   }
-});
+})
