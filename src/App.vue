c145c21ac4611550d61f3141f4b9058826238b90
Neras <medianeras57@gmail.com>
2019-01-08 00:16:51 +0800

    sth new

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="navs">
+      <new-connect />
+      <connected-tree />
+      <history />
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import ConnectedTree from '@/components/Connected/index.vue'
+import NewConnect from '@/components/NewConnect/index.vue'
+import History from '@/components/History/index.vue'
+
+@Component({
+  components: {
+    ConnectedTree,
+    NewConnect,
+    History
+  }
+})
+export default class App extends Vue {
+}
+</script>
 
 <style lang="less">
 html, body {
@@ -15,10 +39,17 @@ html, body {
   box-sizing: border-box;
   width: 100%;
   height: 100%;
+  display: flex;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-#nav{
-  width: 500px;
+.navs{
+  width: 200px;
+  min-width: 200px;
+}
+.content{
+  width: 100%;
+  box-sizing: border-box;
+  padding: 20px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import store from './store'
+import store from './store/index'
 
 declare global {
   interface Window { require: any; }
