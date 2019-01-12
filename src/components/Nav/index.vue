f97ce7d3b63ee0e2f0b2454052e21bce3a291b20
Neras <medianeras57@gmail.com>
2019-01-13 00:15:32 +0800

    add Nav component

---
+++ src/components/Nav/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <new-connect />
+    <connected />
+    <history />
+  </div>
+</template>
+
+<script lang="ts">
+import Connected from '@/components/Connected/index.vue'
+import History from '@/components/History/index.vue'
+import NewConnect from '@/components/NewConnect/index.vue'
+import {
+  Vue,
+  Component
+} from 'vue-property-decorator'
+@Component({
+  components: {
+    Connected,
+    History,
+    NewConnect
+  }
+})
+export default class Nav extends Vue {}
+</script>
+
+<style lang="less" scoped>
+</style>
