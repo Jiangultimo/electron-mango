1ca81189c48445e9f1c9c9cd863ab9d3b3718c7b
Neras <medianeras57@gmail.com>
2019-01-13 00:15:14 +0800

    adjust code

--- main/action.js
+++ main/action.js
@@ -14,28 +14,31 @@ module.exports = ({
     app.quit() //退出程序
   },
   'delDb'() {
-    if (arg.name in global.shared.dbList) {
-      delete global.shared.dbList[arg.name]
-      jsonStorage.set(storeKey, global.shared.dbList, relaodDb)
+    const { shared: { dbList } } = global
+    if (arg.name in dbList) {
+      delete dbList[arg.name]
+      jsonStorage.set(storeKey, dbList, relaodDb)
     } else {
       event.sender.send('notify', { message: '此连接已删除！' })
     }
   },
   'editDb'() {
-    if (arg.key in global.shared.dbList) {
-      delete global.shared.dbList[arg.key]
-      global.shared.dbList[arg.name] = arg.uri
-      jsonStorage.set(storeKey, global.shared.dbList, relaodDb)
+    const { shared: { dbList } } = global
+    if (arg.key in dbList) {
+      delete dbList[arg.key]
+      dbList[arg.name] = arg.uri
+      jsonStorage.set(storeKey, dbList, relaodDb)
     } else {
       event.sender.send('notify', { message: '此连接已被修改！' })
     }
   },
   'addDb'() {
-    if (arg.name in global.shared.dbList) {
+    const { shared: { dbList } } = global
+    if (arg.name in dbList) {
       event.sender.send('notify', { message: '此连接已存在！' })
     } else {
-      global.shared.dbList[arg.name] = arg.uri
-      jsonStorage.set(storeKey, global.shared.dbList, relaodDb)
+      dbList[arg.name] = arg.uri
+      jsonStorage.set(storeKey, dbList, relaodDb)
     }
   },
   'showAddDb'() {
@@ -45,48 +48,49 @@ module.exports = ({
       openWin('http://localhost:3000/#/database/add')
     }
   },
-  'getCollects'(){
+  'getCollects'() {
+    const { shared: { dbClient } } = global
     console.log('fuck');
-    
-    const client=global.shared.dbClient[arg.link]
+
+    const client = dbClient[arg.link]
     client.db(arg.db).collections()
-    .then((val)=>{
-      arg.data=val
-      event.sender.send('mongoRes', arg)
-    })
+      .then((val) => {
+        arg.data = val
+        event.sender.send('mongoRes', arg)
+      })
   },
   'connect'() {
+    const { shared: { dbList, dbClient } } = global
     const { name } = arg
-    const { dbList } = global.shared
     const client = new MongoClient(dbList[name], { useNewUrlParser: true })
     client.connect((err) => {
-      if(err) {
+      if (err) {
         handleError(err, event)
         return false
       }
-      let dbName='admin'
-      let str=dbList[name].substr(11)
-      var i = str.indexOf('/')+1
-      if (i>0){
-        if (str.indexOf('?')>0){
-          dbName=str.substring(i,str.indexOf('?'))
-        }else dbName=str.substring(i)
+      let dbName = 'admin'
+      let str = dbList[name].substr(11)
+      var i = str.indexOf('/') + 1
+      if (i > 0) {
+        if (str.indexOf('?') > 0) {
+          dbName = str.substring(i, str.indexOf('?'))
+        } else dbName = str.substring(i)
       }
       // Use the admin database for the operation
       const adminDB = client.db(dbName).admin()
       // List all the available databases
       adminDB.listDatabases()
-      .then((res) => {
-        event.sender.send('connected', { name, dbs: res.databases })
-      })
-      .catch(() => {
-        client.db(dbName).stats()
-        .then((val) => {
-          let allDBs=[{'name':val.db,'sizeOnDisk':val.dataSize}]
-          event.sender.send('connected', { name, dbs: allDBs })
+        .then((res) => {
+          event.sender.send('connected', { name, dbs: res.databases })
         })
-      })
-      global.shared.dbClient[name]=client
+        .catch(() => {
+          client.db(dbName).stats()
+            .then((val) => {
+              let allDBs = [{ 'name': val.db, 'sizeOnDisk': val.dataSize }]
+              event.sender.send('connected', { name, dbs: allDBs })
+            })
+        })
+      dbClient[name] = client
     })
   }
 })
